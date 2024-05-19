050b00648590532cd2f3de874351a9c74af588f1
Ajoy Deb Nath <57573642+Ajoy-1704001@users.noreply.github.com>
2024-05-19 21:37:21 +0600

    updated agora app id

--- lib/config/agora_config.dart
+++ lib/config/agora_config.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class AgoraConfig {
-  static const String appId = '79ffc2d7dbc7434da06ad1351535dc85';
-  static const String token =
-      '007eJxTYIi8ExdSv0D9w7kVCrs/Z3Vma217mjZx9cfpfK2npsTfmZCpwGBumZaWbJRinpKUbG5ibJKSaGCWmGJobGpoamyakmxh2r7ZPa0hkJHB4NAOFkYGCATxORnK8jOTU5MTc3IYGADFAyP+';
+  static const String appId = '820678d27dd349c2a7c758d1677c18d6';
+  // static const String token =
+  //     '007eJxTYJimt+N7ysZu9xl2drZON9TbXCa+8l+36mQfR2+hWYU8Q4sCg7llWlqyUYp5SlKyuYmxSUqigVliiqGxqaGpsWlKsoXpPX6vtIZARobU8hQmRgYIBPE5GJITc3JKUotLGBgAgnofpA==';
 
   late RtcEngine engine;
   int? _remoteUid;
@@ -15,25 +15,36 @@ class AgoraConfig {
     if (defaultTargetPlatform == TargetPlatform.android) {
       await [Permission.microphone].request();
     }
+    print("Agora Intialized................................");
     RtcEngineContext context = RtcEngineContext(appId);
     engine = await RtcEngine.createWithContext(context);
     await engine.enableAudio();
     engine.setEventHandler(RtcEngineEventHandler(
       joinChannelSuccess: (channel, uid, elapsed) {
-        print('joinChannelSuccess $channel $uid');
+        print('###################joinChannelSuccess $channel $uid');
       },
+      requestToken: () {},
       userJoined: (uid, elapsed) {
-        print('userJoined $uid');
+        print('@@@@@@@@@@@@@@@@@@@@@userJoined $uid');
         _remoteUid = uid;
       },
       userOffline: (uid, reason) {
-        print('userOffline $uid');
+        print('***************************userOffline $uid');
+      },
+      error: (err) {
+        print("!!!!!!!!!!!!!!!!!!!!!!!!${err.index}" + err.name);
+      },
+      apiCallExecuted: (error, api, result) {
+        print(")))))))))))))))))))))");
+        print(error.name);
+        print(api);
+        print(result);
       },
     ));
   }
 
   Future<void> joinChannel(String channelName) async {
-    await engine.joinChannel(token, channelName, null, 0);
+    await engine.joinChannel(null, channelName, null, 0);
   }
 
   Future<void> leaveChannel() async {
